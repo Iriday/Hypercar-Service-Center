7792691680b69c1864e11ea73608af245befa583
Igor <GI_Something@protonmail.com>
2020-06-11 00:12:49 +0300

    feat: Impl simple welcome page ("/welcome")

--- hypercar/urls.py
+++ hypercar/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ticket.views import WelcomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/', WelcomeView.as_view()),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
 
 # Create your views here.
+
+class WelcomeView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("<h2>Welcome to the Hypercar Service!<h2>")
